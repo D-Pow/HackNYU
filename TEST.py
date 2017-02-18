87fb699a0ee5fe35bf3edc495f23fe57d8aa5279
Sheldon Jatou <sheldonjatou@gmail.com>
2017-02-17 21:47:58 -0500

    Draw
    The program now draws lines, but draws from (0,0) and draws straight lines to the next click.

--- TEST.py
+++ TEST.py
@@ -1,16 +1,22 @@
 import pygame
+
 pygame.init()
 Canvas = pygame.display.set_mode((500,500))
 pygame.display.set_caption("TEST")
 clock = pygame.time.Clock()
 closed = False
 
+start_pos = (0,0)
 while not closed:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             closed = True
-    print(event)
-       
+
+    if pygame.mouse.get_pressed() == (1,0,0):
+        end_pos = pygame.mouse.get_pos()
+        pygame.draw.line(Canvas, (255,255,255), start_pos, end_pos,5)
+        start_pos = end_pos
+    
     pygame.display.update()
     clock.tick(60)
     
