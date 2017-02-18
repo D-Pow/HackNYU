903153049b9d73af465d2d2838a5600efd985eb8
Sheldon Jatou <sheldonjatou@gmail.com>
2017-02-17 23:48:13 -0500

    Drawing Pad working
    drawing works, next step is to implement erasing

--- TEST.py
+++ TEST.py
@@ -2,23 +2,28 @@ import pygame
 
 pygame.init()
 Canvas = pygame.display.set_mode((500,500))
-pygame.display.set_caption("TEST")
+pygame.display.set_caption("DRAWING PAD")
 clock = pygame.time.Clock()
 closed = False
 
-start_pos = (0,0)
+start_pos = None
 while not closed:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             closed = True
-
     if pygame.mouse.get_pressed() == (1,0,0):
         end_pos = pygame.mouse.get_pos()
-        pygame.draw.line(Canvas, (255,255,255), start_pos, end_pos,5)
+        if start_pos:
+            pygame.draw.line(Canvas, (255,255,255), start_pos, end_pos,5)
+        else:
+            pygame.draw.line(Canvas, (255,255,255), end_pos, end_pos,5)
         start_pos = end_pos
+    if pygame.mouse.get_pressed() == (0,0,0):
+        start_pos = None
     
     pygame.display.update()
     clock.tick(60)
     
 pygame.quit()
 quit()
+        
